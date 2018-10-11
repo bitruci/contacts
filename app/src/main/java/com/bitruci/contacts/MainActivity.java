74b1e159598ca4953696d5d082c9c1b2eac33b93
Selim Parmaksız <yselim.iletisim@gmail.com>
2018-10-11 22:32:04 +0300

    düzelt

--- app/src/main/java/com/bitruci/contacts/MainActivity.java
+++ app/src/main/java/com/bitruci/contacts/MainActivity.java
@@ -10,13 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    void test(){
-
-    }
-
-    void test2() {
-
-    }
+    
 
 }
